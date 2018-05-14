633d3ab46651112929138b9b7995586b31ba814e
IV <ivan_vasilyev@inbox.ru>
2018-05-14 16:11:32 +0400

    user pass1

--- app_server/controllers/ctrlUsers.js
+++ app_server/controllers/ctrlUsers.js
@@ -1,33 +1,43 @@
+const crypto = require('crypto');
 const User = require('../models/users');
 
-module.exports = {
-  async list(req, res, next) {
-    res.locals.user = req.user;
-    
+const list = async(req, res, next) => {
+  res.locals.user = req.user;
+  
+  try {
+    res.locals.users = await User.find({}).exec();;
+  } catch (error) {
+    res.locals.error = error.message;
+  }
+  res.render('users');
+}
+
+// Исправить add
+const add = async(req, res, next) => {
+  if (req.body.username && req.body.password && req.body.email) {
+    const salt = new Buffer(
+      crypto.randomBytes(16).toString('base64'), 
+      'base64'
+    );
+
+    const newUser = new User({
+      username: req.body.username,
+      password: crypto.pbkdf2Sync(
+        req.body.password, salt, 1000, 64).toString('base64'),
+      email: req.body.email,
+      salt: salt
+    });
+
     try {
-      res.locals.users = await User.find({}).exec();;
+      await newUser.save();
     } catch (error) {
       res.locals.error = error.message;
     }
-    res.render('users');
-  },
-  // Исправить add
-  async add(req, res, next) {
-    if (req.body.username && req.body.password && req.body.email) {
-      const salt = '1111';
-      const newUser = new User({
-        username: req.body.username,
-        password: req.body.password + salt,
-        email: req.body.email,
-        salt: user.salt
-      });
-  
-      try {
-        await newUser.save();
-      } catch (error) {
-        res.locals.error = error.message;
-      }
-      res.redirect('/users');
-    }
+    res.redirect('/users');
   }
+}
+
+module.exports = {
+  list,
+  add
 };
